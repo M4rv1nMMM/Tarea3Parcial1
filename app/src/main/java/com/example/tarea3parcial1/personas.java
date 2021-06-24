c63c42984067f0a8f545ddc031d53f486ed76605
M4rv1nMMM <marvinmiguel3@gmail.com>
2021-06-23 22:40:35 -0600

    Creacion de Clase personas y creacion de base de datos y tabla

---
+++ app/src/main/java/com/example/tarea3parcial1/personas.java
@@ -0,0 +1,23 @@
+package com.example.tarea3parcial1;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class personas extends SQLiteOpenHelper {
+
+    public personas(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tbl_personas(pname text primary key, pname2 text, pedad int, pmail text, paddress text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
